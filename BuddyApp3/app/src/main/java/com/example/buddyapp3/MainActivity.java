0e25d7d9a32a3fbb0e28ce4f47eb743a462be583
sevanahmadzada <55389008+sevanahmadzada@users.noreply.github.com>
2019-11-28 17:38:17 +0900

    Link to the next page

--- BuddyApp3/app/src/main/java/com/example/buddyapp3/MainActivity.java
+++ BuddyApp3/app/src/main/java/com/example/buddyapp3/MainActivity.java
@@ -2,16 +2,29 @@ package com.example.buddyapp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView registerLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        registerLink = (TextView) findViewById(R.id.textRegister);
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(MainActivity.this,subscription.class);
+                startActivity(i);
+            }
+
+                             }
+
+        );
+    }
 }
